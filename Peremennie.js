4123efa01564ee9f3d804a3ac2d86bf13e061930
Alexsander Supranovich <sa6ka90@gmail.com>
2020-03-26 19:54:39 +0300

    commit massege

---
+++ Peremennie.js
@@ -0,0 +1,27 @@
+// Задача 1
+let num = '3';
+console.log(num);
+// Задача 2
+let a = 10;
+let b = 2;
+alert(a+b)
+alert(a-b)
+alert(a*b)
+alert(a/b)
+// Задача 3
+let c = 15;
+let d = 2;
+let result = c+d;
+console.log(result)
+// Задача 4, можно сделать так же как и в задаче №3
+let a = 10;
+let b = 2;
+let c = 5;
+console.log(a+b+c)
+// Задача 5
+let a = 17;
+let b = 10;
+let c = a-b;
+let d = c;
+let result = c+d;
+console.log(result)
