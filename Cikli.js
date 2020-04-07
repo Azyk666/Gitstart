207910ece55b6080d2e1668a95a65d4eecabf48e
Alexsander Supranovich <sa6ka90@gmail.com>
2020-04-08 01:00:07 +0300

    (Cikli)

---
+++ Cikli.js
@@ -0,0 +1,60 @@
+// 1)Выведите столбец чисел от 1 до 100.
+let out = document.getElementById ('out');
+/* let i = 1;
+
+while (i < 100) {
+    out.innerHTML += i + '<br>';
+    i++;
+} */
+
+/* for (let i = 1 ; i < 100; i++) {
+    out.innerHTML += i + '<br>';
+} */
+
+// 2)Выведите столбец чисел от 11 до 33.
+/* let i = 11;
+
+while (i < 33) {
+    out.innerHTML += i + '<br>';
+    i++;
+} */
+
+/* for (let i = 11 ; i < 33; i++) {
+    out.innerHTML += i + '<br>';
+} */
+
+// 3)Выведите столбец четных чисел в промежутке от 0 до 100.
+/* let i = 1;
+
+while (i < 100) {
+    if (i % 2 == 0){
+        out.innerHTML += i + '<br>';
+    }
+    i++;
+} */
+
+
+/* for (let i = 1 ; i <= 100; i++) {
+    if (i % 2 == 0) {
+        out.innerHTML += i + '<br>';
+    }
+} */
+
+// 4)С помощью цикла найдите сумму чисел от 1 до 100.
+/* let i = 0;
+let y = 0;
+
+do {
+    y += i;
+    i++;
+} while ( i <= 100 );
+
+out.innerHTML = y; */
+
+/* let y = 0;
+
+for (let i = 0 ; i <= 100; i++) {
+    y += i;
+}
+
+out.innerHTML = y; */
